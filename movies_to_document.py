af7db3ec678c7867d272e45c415bf0b58d22c651
felixHelloFresh <felixmooij@hellofresh.nl>
2021-03-26 10:31:36 +0100

    JSON key's no longer contain '$'

--- movies_to_document.py
+++ movies_to_document.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 def create_size_independent_tables():
     all_users = pd.read_json("mflix_users.json")
+    all_users['_id'] = all_users["_id"].apply(lambda x: {'oid': x["$oid"]})
+
     all_sessions = pd.read_json("mflix_sessions.json")
+    all_sessions['_id'] = all_sessions["_id"].apply(lambda x: {'oid': x["$oid"]})
 
     return all_users, all_sessions
 
@@ -12,6 +15,7 @@ def create_movie_info(num_rows: int):
 
     ##Creation of movie_info csv
     all_movies = pd.read_json("mflix_movies.json").iloc[0:num_rows, :]
+    all_movies['_id'] = all_movies["_id"].apply(lambda x: {'oid': x["$oid"]})
 
     return all_movies
 
@@ -21,9 +25,10 @@ def create_comments(all_movies):
     ###### Comments csv
     all_comments = pd.read_json("mflix_comments.json")
     all_comments['join_id'] = all_comments["movie_id"].apply(lambda x: x["$oid"])
+    all_comments['_id'] = all_comments["_id"].apply(lambda x: {'oid': x["$oid"]})
 
     # only select comments from the relevant movies
-    all_movies = all_movies["_id"].apply(lambda x: x["$oid"])
+    all_movies = all_movies["_id"].apply(lambda x: x["oid"])
     all_comments = all_comments.loc[all_comments["join_id"].isin(all_movies)]
 
     return all_comments
